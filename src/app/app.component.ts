4082fc026cb26497cf7bc428b6bc5f83a39551df
emnekola <58869230+emnekola@users.noreply.github.com>
2020-10-27 11:40:37 -0500

    Removed dead code
    Wasn't using this code anymore!

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,10 +7,4 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Quiz Editor';
-
-  badger = {
-    color1: 'red',
-    color2: 'white',
-    padding: '20px'
-  };
 }
