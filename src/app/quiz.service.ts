a51eb9ae42da4a2a0d1363341c2d089d8e3340b0
salina225 <skhanom@madisoncollege.edu>
2020-11-03 11:29:42 -0600

    added questions

--- src/app/quiz.service.ts
+++ src/app/quiz.service.ts
@@ -1,9 +1,38 @@
 import { Injectable } from '@angular/core';
 
+export interface QuizDisplay {
+  name: string
+  , numberOfQuiestions: number;
+}
+
+
+
 @Injectable({
   providedIn: 'root'
 })
+
+
 export class QuizService {
 
   constructor() { }
+
+  fetchQuizzes(): QuizDisplay[] {
+
+  return [
+  {
+    name: 'Quiz 1'
+  , numberOfQuiestions: 5
+},
+{
+  name: 'Quiz 2'
+  , numberOfQuiestions: 0
+},
+{
+  name: 'Quiz 1'
+  , numberOfQuiestions: 10
+}
+  ];
+
+}
+
 }
