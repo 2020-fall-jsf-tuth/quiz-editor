917eee0ecbb086375e43a67faa0ec78c02dd6134
gkatuwal@madisoncollege.edu <Kurkuntar1>
2020-11-03 11:45:50 -0600

    no quizz behaviour

--- src/app/quiz.service.ts
+++ src/app/quiz.service.ts
@@ -1,9 +1,30 @@
 import { Injectable } from '@angular/core';
-
+export interface QuizDisplay{
+  name:string;
+  numberOfQuestion: number;
+}
 @Injectable({
   providedIn: 'root'
 })
 export class QuizService {
 
   constructor() { }
+
+  fetchQuizzes():QuizDisplay[] {
+    return [
+      {
+        name:'Quiz 1',
+        numberOfQuestion: 5
+      },
+      {
+        name:'Quiz 2',
+        numberOfQuestion: 0
+      },
+      {
+        name:'Quiz 3',
+        numberOfQuestion: 10
+      }
+    ];
+  }
+
 }
