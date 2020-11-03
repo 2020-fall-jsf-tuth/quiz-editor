038ff5d99d5252e095ef28f21554e485a9fbded5
tsteelemadisoncollege <tsteele@madisoncollege.edu>
2020-11-03 11:27:30 -0600

    Showing quizzes ! ! !

--- src/app/quiz.service.ts
+++ src/app/quiz.service.ts
@@ -1,9 +1,34 @@
 import { Injectable } from '@angular/core';
 
+export interface QuizDisplay {
+  name: string;
+  numberOfQuestions: number;
+}
+
 @Injectable({
   providedIn: 'root'
 })
 export class QuizService {
 
   constructor() { }
+
+  fetchQuizzes(): QuizDisplay[] {
+
+    return [
+      {
+        name: 'Quiz 1'
+        , numberOfQuestions: 5
+      }
+      , {
+        name: 'Quiz 2'
+        , numberOfQuestions: 0
+      }
+      , {
+        name: 'Quiz 3'
+        , numberOfQuestions: 10
+      }
+    ];
+
+  }
+
 }
