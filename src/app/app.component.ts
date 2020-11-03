ff57a2a27e0207c116db208e5081468da7ee86ce
mmiatech <mmiatech@madisoncollege.edu>
2020-11-03 11:27:05 -0600

    showing quizzes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { QuizService, QuizDisplay } from './quiz.service';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,12 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'quiz-editor';
+
+  quizzes: QuizDisplay[] = [];
+
+  constructor(private quizSvc: QuizService) {
+    //fetch quizzes here
+    this.quizzes = this.quizSvc.fetchQuizzes();
+  }
+
 }
