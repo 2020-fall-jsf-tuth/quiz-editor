645d4a579b8e4c67baa35b8e68e18a905f1ed4a8
pugDad26 <63824751+pugDad26@users.noreply.github.com>
2020-11-03 19:47:55 -0600

    Added add new quiz functionality

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -17,10 +17,30 @@ export class AppComponent {
     this.quizzes = this.quizSvc.fetchQuizzes();
   }
 
+  //Don't show quiz information unless a user selects the quiz
   selectedQuiz: QuizDisplay = undefined;
 
+  //Show quiz object information when user clicks on quiz
   setSelectedQuiz(quizToSelect: QuizDisplay) {
     this.selectedQuiz = quizToSelect;
+  }
+
+  addQuiz() {
+    //Creating a new quiz object
+    const newQuiz = {
+      name: "Untitled Quiz",
+      numberOfQuestions: 0
+    };
+
+    //Take current quiz list objects and spread in all current 
+    //quiz list and add the newly created quiz
+    this.quizzes = [
+      ...this.quizzes,
+      newQuiz
+    ];
+
+    //Make new quiz the selected quiz to edit by calling setSelectedQuiz(send it new quiz)
+    this.setSelectedQuiz(newQuiz);
   }
 
 }
