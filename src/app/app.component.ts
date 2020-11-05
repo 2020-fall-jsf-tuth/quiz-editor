4d8df7f54e2af552c680d5c628f946dafcbbffd2
tsteelemadisoncollege <tsteele@madisoncollege.edu>
2020-11-05 11:54:12 -0600

    Displaying quizzes from the cloud ! ! !

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -19,6 +19,10 @@ export class AppComponent {
       .subscribe(
         (data) => {
           console.log(data);
+          this.quizzes = (data as any).map(x => ({
+            name: x.name
+            , questions: x.questions
+          }));
         }
         , (err) => {
           console.error(err);
@@ -44,7 +48,7 @@ export class AppComponent {
   addNewQuiz() {
     const newQuiz = {
       name: "Untitled Quiz"
-      , numberOfQuestions: 0
+      , questions: []
     };
 
     this.quizzes = [
