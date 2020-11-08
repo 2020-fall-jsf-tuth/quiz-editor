f88c7f6f800b2e74b263af5a18d509b4d3184942
albertmensah <DMensah@madisoncollege.edu>
2020-11-07 19:48:43 -0600

    first observable working
    See data coming back from the cloud

--- src/app/quiz.service.ts
+++ src/app/quiz.service.ts
@@ -1,4 +1,6 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
 
 export interface QuizDisplay {
   name: string;
@@ -10,24 +12,11 @@ export interface QuizDisplay {
 })
 export class QuizService {
 
-  constructor() { }
+  constructor(private builtInAngularHttpClient: HttpClient) { }
 
-  fetchQuizzes(): QuizDisplay[] {
+  fetchQuizzes(): Observable<any> {
 
-    return [
-      {
-        name: 'Quiz 1'
-        , numberOfQuestions: 5
-      }
-      , {
-        name: 'Quiz 2'
-        , numberOfQuestions: 1
-      }
-      , {
-        name: 'Quiz 3'
-        , numberOfQuestions: 10
-      }
-    ];
+    return this.builtInAngularHttpClient.get("https://modern-js.azurewebsites.net/api/HttpTriggerJS1?code=8XD3vN3ehHLdZacBQJQhgUnNst9202gdd5VM3kWCytDkz2nXhia6kA==&name=Mystery%20Quiz"); 
 
   }
 
