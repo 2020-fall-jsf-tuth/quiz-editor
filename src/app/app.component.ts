ee7bf33422257a5ed6ce2e64879f85e50992ad6a
tsteelemadisoncollege <tsteele@madisoncollege.edu>
2020-12-01 11:20:02 -0600

    Deleted quiz badge working...
    Consider adding tooltip in the future...

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -177,4 +177,12 @@ export class AppComponent implements OnInit {
       console.error(err);
     }
   }
+
+  private getDeletedQuizzes(): QuizDisplay[] {
+    return this.quizzes.filter(x => x.markedForDelete);
+  }
+
+  get DeletedQuizCount(): number {
+    return this.getDeletedQuizzes().length;
+  }
 }
