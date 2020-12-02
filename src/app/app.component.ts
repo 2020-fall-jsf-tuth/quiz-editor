88b7174f3b9523116576adcabe477dc613560e77
mmiatech <mmiatech@madisoncollege.edu>
2020-12-02 14:17:37 -0600

    added badge working

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -39,6 +39,7 @@ export class AppComponent implements OnInit {
             name: x.name
             , questions: x.questions
             , markedForDelete: false
+            , newlyAdded: false
           }));
           
           this.loading = false;
@@ -73,6 +74,7 @@ export class AppComponent implements OnInit {
       name: "Untitled Quiz"
       , questions: []
       , markedForDelete: false
+      , newlyAdded: true
     };
 
     this.quizzes = [
@@ -183,6 +185,14 @@ export class AppComponent implements OnInit {
 
   get DeletedQuizCount(): number {
     return this.getDeletedQuizzes().length;
+  }
+
+  private getAddedQuizzes(): QuizDisplay[] {
+    return this.quizzes.filter(x => x.newlyAdded && !x.markedForDelete);
+  }
+
+  get AddedQuizCount(): number {
+    return this.getAddedQuizzes().length;
   }
 
 }
