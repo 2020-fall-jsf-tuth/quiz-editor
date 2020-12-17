c586d9eeef1e41915d850058bd4c4798615647dd
RigoTheAmigo <rodgodoy93@gmail.com>
2020-12-17 14:23:34 -0600

    Update app.component.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,7 +4,7 @@ import { QuizService, QuizDisplay } from './quiz.service';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'quiz-editor';
@@ -12,23 +12,19 @@ export class AppComponent {
   quizzes: QuizDisplay[] = [];
 
   constructor(private quizSvc: QuizService) {
-
     // Fetch quizzes here ! ! !
-    this.quizSvc
-    .fetchQuizzes()
-    .subscribe(
+    this.quizSvc.fetchQuizzes().subscribe(
       (data) => {
-        console.log(data); 
-        this.quizzes = (data as any).map(x => ({
-          name: x.name
-          , questions: x.questions
-        }));  
+        console.log(data);
+        this.quizzes = (data as any).map((x) => ({
+          name: x.name,
+          questions: x.questions,
+        }));
+      },
+      (err) => {
+        console.log(err);
       }
-      , (err) => {
-        console.log(err); 
-      }
-    )
-    ;
+    );
   }
 
   selectedQuiz: QuizDisplay = undefined;
@@ -36,30 +32,23 @@ export class AppComponent {
   setSelectedQuiz(quizToSelect: QuizDisplay) {
     this.selectedQuiz = quizToSelect;
 
-    setTimeout(
-      () => {
-        console.log(this.autoFocusInput);
-        this.autoFocusInput.nativeElement.select();
-      }
-      , 30
-    );
+    setTimeout(() => {
+      console.log(this.autoFocusInput);
+      this.autoFocusInput.nativeElement.select();
+    }, 30);
   }
 
   addNewQuiz() {
     const newQuiz = {
-      name: "Untitled Quiz"
-      , questions: []
+      name: 'Quizziest Quiz ever!',
+      questions: [],
     };
 
-    this.quizzes = [
-      ...this.quizzes
-      , newQuiz
-    ];
+    this.quizzes = [...this.quizzes, newQuiz];
 
     this.setSelectedQuiz(newQuiz);
   }
 
-  @ViewChild('myInputForAutoFocus') 
+  @ViewChild('myInputForAutoFocus')
   autoFocusInput: any;
-
 }
